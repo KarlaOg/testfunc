8f46bea1113bf0bcf72d6deb359e1fb4fd77debd
KarlaOg <karla.og@supinternet.fr>
2022-05-12 09:33:14 +0200

    Base

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
-const router = express.Router();
+const db = require('./config/config');
 const port = 3000
 
-app.get('/', (req, res)  => {
-  res.send('Hello World!')
+
+app.get('/', (req, res) => {
+  res.send("Hello World")
 })
 
+app.get('/testDB', (req, res) => {
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
