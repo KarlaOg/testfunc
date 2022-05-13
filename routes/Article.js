8aa0c3e78d01d66b225fbba6336805ab52210096
Coumba Camara <coumba.camara75@gmail.com>
2022-05-13 09:22:49 +0200

    remove console log

--- routes/Article.js
+++ routes/Article.js
@@ -10,8 +10,39 @@ router.get("", async (req,res) =>{
 router.post("", async (req,res) => {
     try{
         const article = await Article.create(req.body);
-        console.log(article)
         res.status(201).send(article);
+    }
+    catch (error){
+        if (error.name === "SequelizeValidationError"){
+            res.status(400).send(error.message);
+        }
+        else{
+            res.sendStatus(500);
+            console.error(error)
+        }
+    }
+});
+
+router.patch("", async (req,res) => {
+    try{
+        const article = await Article.update(req.body,{where: req.query});
+        res.status(200).send(article);
+    }
+    catch (error){
+        if (error.name === "SequelizeValidationError"){
+            res.status(400).send(error.message);
+        }
+        else{
+            res.sendStatus(500);
+            console.error(error)
+        }
+    }
+});
+
+router.post("", async (req,res) => {
+    try{
+        const article = await Article.destroy({where: req.query});
+        res.status(200)
     }
     catch (error){
         if (error.name === "SequelizeValidationError"){
